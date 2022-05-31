e72e4b8bbd03e295f55a219ce73d1b33ed3a5914
Ye <wenjiey@golubh3.campuscluster.illinois.edu>
2022-05-30 19:33:11 -0500

    add run.sh

---
+++ run.sh
@@ -0,0 +1,4 @@
+python demo/pcd_demo.py \
+demo/data/kitti/kitti_000008.bin \
+configs/pointpillars/hv_pointpillars_secfpn_6x8_160e_kitti-3d-car.py \
+checkpoints/hv_pointpillars_secfpn_6x8_160e_kitti-3d-car_20220331_134606-d42d15ed.pth
